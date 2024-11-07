11570af36573399288e0cb958a6cea728d83149b
Yanan LYU <arol666@hotmail.com>
2024-11-07 13:39:27 +0100

    style: add a space between year and name

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
-    return (
-        <footer>
-            <div className="flex justify-center text-sm text-gray-500 p-1">
-                © {new Date().getFullYear()} <a href="https://github.com/lyuya">Yanan LYU</a>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="flex justify-center text-sm text-gray-500 p-1">
+        © {new Date().getFullYear()} &nbsp;
+        <a href="https://github.com/lyuya">Yanan LYU</a>
+      </div>
+    </footer>
+  );
 }
